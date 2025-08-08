149b762afa7f2991edcc4582ab2957d0bc1bbd39
Anuda Attanayake <anudaattanayake@gmail.com>
2025-08-08 22:43:58 +0530

    gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Home from './pages/Home';
 
 export default function App() {
   return (
-      <Router>
-        <Routes>
-
-          <Route index element={<Home />} />
-             
-        </Routes>
-      </Router>
+    <Router basename="/adzener-landing">
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
